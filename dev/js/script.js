5a63360d2d8c514a142cad5797edd1b54d935d11
Ferreira Horta Manuel <manuelferreirahorta@gmail.com>
2023-05-04 10:16:53 +0200

    todo contact

--- dev/js/script.js
+++ dev/js/script.js
@@ -14,3 +14,24 @@ const swiper = new Swiper('.header__swiper', {
     prevEl: '.swiper-button-prev',
   },
 })
+
+// Sticky Script on nav
+// let nav = document.querySelector('.main__nav');
+let opacitySwitch = document.querySelector('.main__nav');
+let sticky = 100;
+let goTop = 0;
+document.addEventListener('scroll', () => {
+    let top = document.documentElement.scrollTop;
+    if(window.pageYOffset > sticky){
+			// nav.style.opacity = 0;
+			opacitySwitch.style.opacity = 0;
+        if(goTop < top){
+					// nav.style.opacity = 0;
+					opacitySwitch.style.opacity = 0;
+        }else{
+					// nav.style.opacity = 1;
+					opacitySwitch.style.opacity = 1;
+        }
+    }
+    goTop = top;
+});
